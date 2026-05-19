a0dc8d079ad3ebfea6c66981374ce99fdccc1d0a
PrashantOmBairagi <prashantbairagi2018@gmail.com>
2026-05-19 17:42:39 +0530

    Add Springboot Starter files 19-MAY-2026

---
+++ src/main/java/com/prashant/smartfinancetracker/SmartFinanceTrackerAppApplication.java
@@ -0,0 +1,13 @@
+package com.prashant.smartfinancetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartFinanceTrackerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartFinanceTrackerAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prashant/smartfinancetracker/SmartFinanceTrackerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prashant.smartfinancetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartFinanceTrackerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
