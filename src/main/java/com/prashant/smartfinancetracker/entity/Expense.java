88602bbc1cc1414811dc64d7ff86c7c09230688d
PrashantOmBairagi <prashantbairagi2018@gmail.com>
2026-05-28 18:22:27 +0530

    fIX: add lombok getter setter on expense entity.

--- src/main/java/com/prashant/smartfinancetracker/entity/Expense.java
+++ src/main/java/com/prashant/smartfinancetracker/entity/Expense.java
@@ -12,45 +12,34 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "expenses")
 public class Expense {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Getter
-    @Setter
+
     private Long id;
 
-    @Getter
-    @Setter
     @Column(length = 100)
     private String description;
 
-    @Getter
-    @Setter
     @Column(precision = 10, scale = 2)
     @NotNull
     @Positive(message = "Amount must be positive.")
     private BigDecimal amount;
 
-    @Getter
-    @Setter
     @Enumerated(EnumType.STRING)
     @NotNull(message = "Category is required.")
     private ExpenseCategory category ;
 
-    @Getter
-    @Setter
     @NotNull(message = "Expense date is required.")
     @PastOrPresent(message = "Expense cannot have future date.")
     private LocalDate expenseDate;
 
-    @Getter
-    @Setter
     private LocalDateTime createdAt;
 
-    @Getter
-    @Setter
     private LocalDateTime updatedAt;
 
 //    @Getter
